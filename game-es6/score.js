16cee99f87b72575c8445c30bf7b3ba504fa355c
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-03-02 20:45:05 +0530

    Added net/divider and positioned scores

--- game-es6/score.js
+++ game-es6/score.js
@@ -1,13 +1,15 @@
 function Score(game) {
-    this.game = game;
-    this.score_player1 = document.getElementById("score_player1");
-    this.score_player2 = document.getElementById("score_player2");
+  this.game = game;
+  this.score_player1 = document.getElementById("score_player1");
+  this.score_player2 = document.getElementById("score_player2");
 
-    this.score_player1.style.left = game.width / 2 - 100 + "px"; //game.width * 3 / 8;
-    this.score_player1.style.top = 50 + "px";
+  this.score_player1.style.left = game.width / 2 - 50 - 10 + "px"; //game.width * 3 / 8;
+  this.score_player1.style.top = 50 + "px";
+
+  this.score_player2.style.left = game.width / 2 + 10 + "px";
 }
 
-Score.prototype.draw = function () {
-    this.score_player1.innerHTML = "ME: " + this.game.player.score; // Player score;
-    this.score_player2.innerHTML = "BOT: " + this.game.bot.score; // Bot score
-}
+Score.prototype.draw = function() {
+  this.score_player1.innerHTML = "ME: " + this.game.player.score; // Player score;
+  this.score_player2.innerHTML = "BOT: " + this.game.bot.score; // Bot score
+};
--- game-es6/start.js
+++ game-es6/start.js
@@ -1,13 +1,20 @@
 var canvas = document.getElementById("game"),
-    game = new Game(canvas);
+  game = new Game(canvas),
+  divider = document.getElementById("divider");
 
-// TODO: Complete the start and game function
+// Position the divider
+let width = canvas.offsetWidth;
+let height = canvas.offsetHeight;
+
+let centerX = width / 2;
+divider.style.left = centerX + "px";
+divider.style.height = height + "px";
 
 game.entities = [
-    game.player = new Player(game),
-    game.bot = new Bot(game),
-    game.ball = new Ball(game),
-    game.score = new Score(game)
+  (game.player = new Player(game)),
+  (game.bot = new Bot(game)),
+  (game.ball = new Ball(game)),
+  (game.score = new Score(game))
 ];
 
 game.start(); // Start will trigger game loop
